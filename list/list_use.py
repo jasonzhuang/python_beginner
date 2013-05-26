d0492e4139d0079579efd90d16be7e9ac3a46ba8
jasonzhuang <bluezyg@gmail.com>
2013-05-26 17:04:00 +0800

    add class and list

---
+++ list/list_use.py
@@ -0,0 +1,10 @@
+def even_square():
+	even_squares = [x**2 for x in range(1,11) if x%2 == 0]
+	return even_squares
+
+def slice_list():
+	to_21 = range(1,22)
+	odds = to_21[0::2]
+	middle_third = to_21[7:14:]
+	print odds
+	print middle_third
